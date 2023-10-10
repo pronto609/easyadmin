117da66f664022de96b3b39b04a32ea9da08ba43
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-10 13:38:06 +0300

    formate query builder on assosiation field

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Question;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
@@ -27,7 +28,19 @@ class QuestionCrudController extends AbstractCrudController
             ->hideOnIndex();
         yield Field::new('votes', 'Total Votes')
         ->setTextAlign('right');
-        yield AssociationField::new('askedBy');
+        yield AssociationField::new('askedBy')
+        ->autocomplete()
+        ->formatValue(static function ($value, Question $question) {
+            if (!$user =$question->getAskedBy()) {
+                return null;
+            }
+
+            return sprintf('%s&nbsp;(%s)', $user->getEmail(), $user->getQuestions()->count());
+        })
+        ->setQueryBuilder(function (QueryBuilder $queryBuilder) {
+            $queryBuilder->andWhere('entity.enabled = :enabled')
+                ->setParameter('enabled', true);
+        });
         yield Field::new('createdAt')
         ->hideOnForm();
 
