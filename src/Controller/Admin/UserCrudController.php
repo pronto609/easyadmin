b00ca5f53a7f29acdc0c192f1a574d020ef58f1a
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-10 11:27:19 +0300

    configuration fields

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,12 +4,15 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -32,5 +35,11 @@ class UserCrudController extends AbstractCrudController
         ->renderAsSwitch(false);
         yield DateField::new('createdAt')
         ->hideOnForm();
+        $roles = ['ROLE_SUPER_ADMIN', 'ROLE_ADMIN', 'ROLE_MODERATOR', 'ROLE_USER'];
+        yield ChoiceField::new('roles')
+        ->setChoices(array_combine($roles, $roles))
+        ->allowMultipleChoices()
+        ->renderExpanded()
+        ->renderAsBadges();
     }
 }
